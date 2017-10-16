821dc377d34a4ef18cfb10475359b28e43acbc21
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 22:54:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_style = []
+  kesha_style << array.each {|word| word.gsub(2, "$")}
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n}
+end
+
+def add_s(array)
+  [1,2].collect.with_index{|element, index| }
+end
