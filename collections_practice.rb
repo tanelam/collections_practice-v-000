d205e095b8dcf2e75d23c50cf18d6d2b852660fb
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-18 02:48:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,9 +12,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-    array[2], array[1] = array[1], array[2]
+    array = array[2], array[1] = array[1], array[2]
     return array
-
 end
 
 def reverse_array(array)
